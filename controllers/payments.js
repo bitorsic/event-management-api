331f48b55403dc8d29fedb076d35a2e2176a9d47
bitorsic <bitorsic@gmail.com>
2024-12-05 10:56:23 +0530

    payment implementation complete

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,21 +1,11 @@
 const Razorpay = require("razorpay");
+const { validateWebhookSignature } = require('razorpay/dist/utils/razorpay-utils')
 const { users, events, payments } = require("../models");
 
 const razorpay = new Razorpay({
 	key_id: process.env.RAZORPAY_ID,
 	key_secret: process.env.RAZORPAY_SECRET,
 });
-
-const randomStringGenerator = () => {
-	const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-	let string = '';
-
-	for (let i = 0; i < 6; i++) {
-		string += characters.charAt(Math.floor(Math.random() * characters.length));
-	}
-	
-	return string;
-}
 
 const join = async (req, res) => {
 	try {
@@ -60,6 +50,64 @@ const join = async (req, res) => {
 	}
 }
 
+const webhook = async (req, res) => {
+	try {
+		const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET;
+		const webhookSignature = req.headers['x-razorpay-signature'];
+
+		const isSignatureValid = validateWebhookSignature(
+			JSON.stringify(req.body), webhookSignature, webhookSecret
+		);
+
+		if (!isSignatureValid) {
+			console.log("Webhook signature was invalid");
+			return;
+		}
+		
+		// handle all unnecessary cases by just sending ok
+		if (
+			req.body.event !== 'payment.captured' &&
+			req.body.event !== 'payment.failed'
+		) {
+			return res.status(200).send();
+		}
+		
+		console.log(req.body.payload.payment.entity);
+		// find and update payment with the order_id
+		const payment = await payments.findOneAndUpdate(
+			{ orderId: req.body.payload.payment.entity.order_id },
+			{ status: req.body.payload.payment.entity.status },
+		);
+		if (!payment) {
+			return res.status(400).send({ message: "No payment for given order"});
+		}
+
+		// stop if payment has failed
+		if (req.body.event === 'payment.failed') return res.status(200).send();
+
+		// modify event and user documents
+		let event = await events.findById(payment.eventId);
+		event.attendees.push(payment.email);
+			
+		const updatedUser = await users.findByIdAndUpdate(payment.email, {
+			$push: { attending: event._id }
+		});
+		
+		if (!updatedUser) {
+			return res.status(404).send({ message: "User not found" });
+		}
+		
+		await event.save();
+
+		return res.status(200).send({ 
+			message: `Successfully joined the event: ${event.title}`
+		});
+	} catch (e) {
+		res.status(500).send({ message: e.message });
+	}
+}
+
 module.exports = {
 	join,
+	webhook,
 }
--- routes/payments.js
+++ routes/payments.js
@@ -3,5 +3,6 @@ const controller = require("../controllers/payments");
 const verifyToken = require("../middleware/auth");
 
 router.put("/attendance/:id", verifyToken, controller.join);
+router.post("/attendance/webhook", controller.webhook);
 
 module.exports = router;
